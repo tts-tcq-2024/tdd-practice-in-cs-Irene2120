d057ebd1c6847e96bfbf4151dc2e75dd702124ba
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-09-25 18:03:58 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -2,45 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+
 public class StringCalculator
 {
     public int Add(string numbers)
     {
-        // Implementation will go here
-        return 0; // Placeholder return
-    }
+        if (string.IsNullOrEmpty(numbers))
+        {
+            return 0;
+        }
 
-    private string ExtractDelimiter(ref string numbers)
-    {
-        // Implementation will go here
-        return ""; // Placeholder return
-    }
-
-    private string ReplaceNewlines(string numbers, string delimiter)
-    {
-        // Implementation will go here
-        return ""; // Placeholder return
-    }
-
-    private List<int> ExtractNumbers(string input, string delimiter)
-    {
-        // Implementation will go here
-        return new List<int>(); // Placeholder return
-    }
-
-    private void CheckForNegatives(List<int> numbers)
-    {
-        // Implementation will go here
-    }
-
-    private int SumValidNumbers(List<int> numbers)
-    {
-        // Implementation will go here
-        return 0; // Placeholder return
-    }
-
-    private bool IsGreaterThanLimit(int number, int limit)
-    {
-        return number > limit;
+        string[] delimiters = Util.GetDelimiters(numbers, out string numbersWithoutDelimiters);
+        List<int> numberList = Util.ParseNumbers(numbersWithoutDelimiters, delimiters);
+        Util.ValidateNegNums(numberList);
+        return Util.SumCalculator(numberList);
     }
 }
+   
