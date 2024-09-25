03e8eddc5eb3de732e53554d8e04c5a4744994ff
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-09-25 18:32:36 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -5,6 +5,44 @@ using System.Linq;
 
 public class StringCalculator
 {
+
+public static string[] GetDelimiters(string numbers, out string numbersWithoutDelimiters)
+    {
+        string[] delimiters = new string[] { ",", "\n" };
+        numbersWithoutDelimiters = numbers;
+
+        if (numbers.StartsWith("//"))
+        {
+            int delimiterEndIndex = numbers.IndexOf('\n');
+            string customDelimiter = numbers.Substring(2, delimiterEndIndex - 2);
+            delimiters = new string[] { customDelimiter };
+            numbersWithoutDelimiters = numbers.Substring(delimiterEndIndex + 1);
+        }
+        return delimiters;
+    }
+
+    public static List<int> ParseNumbers(string numbers, string[] delimiters)
+    {
+        return numbers.Split(delimiters, StringSplitOptions.None)
+                      .Select(int.Parse)
+                      .ToList();
+    }
+
+    public static void ValidateNegNums(List<int> numbers)
+    {
+        List<int> negativeNumbers = numbers.Where(n => n < 0).ToList();
+        if (negativeNumbers.Any())
+        {
+            throw new Exception("Negative numbers not allowed: " + string.Join(",", negativeNumbers));
+        }
+    }
+
+    public static int SumCalculator(List<int> numbers)
+    {
+        return numbers.Where(n => n <= 1000).Sum();
+    }
+}
+    
     public int Add(string numbers)
     {
         if (string.IsNullOrEmpty(numbers))
@@ -12,10 +50,10 @@ public class StringCalculator
             return 0;
         }
 
-        string[] delimiters = Util.GetDelimiters(numbers, out string numbersWithoutDelimiters);
-        List<int> numberList = Util.ParseNumbers(numbersWithoutDelimiters, delimiters);
-        Util.ValidateNegNums(numberList);
-        return Util.SumCalculator(numberList);
+        string[] delimiters = StringCalculator.GetDelimiters(numbers, out string numbersWithoutDelimiters);
+        List<int> numberList = StringCalculator.ParseNumbers(numbersWithoutDelimiters, delimiters);
+        StringCalculator.ValidateNegNums(numberList);
+        return StringCalculator.SumCalculator(numberList);
     }
 }
    
